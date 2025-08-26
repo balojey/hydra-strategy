bf9835f3dba3a0575c691932153b419aa18cc585
balo <balo@alexandria.pc>
2025-08-26 11:52:17 +0100

    Changed package manager from uv to poetry due to dependency issues. Also connected to broker

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
-def main():
-    print("Hello from hydra-strategy!")
+from os import getenv
+
+from lumibot.strategies import Strategy
+from lumibot.brokers import Alpaca
+from lumibot.traders import Trader
+from lumibot.entities import Asset
 
 
-if __name__ == "__main__":
-    main()
+ALPACA_CONFIG = {
+    'API_KEY': getenv('ALPACA_API_KEY'),
+    'API_SECRET': getenv('ALPACA_API_SECRET'),
+    "PAPER": True if getenv('ALPACA_IS_PAPER', 'true').lower() == 'true' else False,
+}
+
+broker = Alpaca(ALPACA_CONFIG)
+
+class MyStrategy(Strategy):
+    def on_trading_iteration(self):
+        if self.first_iteration:
+            # Buy 100 shares of SPY
+            order = self.create_order(
+                Asset(symbol="SPY", asset_type="stock"),
+                quantity=100,
+                side="buy"
+            )
+            self.submit_order(order)
+
+
+strategy = MyStrategy(broker=broker)
+trader = Trader()
+trader.add_strategy(strategy)
+trader.run_all()
